09e58ad057308ecbec7bde61b5ed9efc7fcad146
Satoru Noguchi <satoru.nog@gmail.com>
2019-05-09 21:07:17 -0300

    Conclusao do Curso

--- jogoteca.py
+++ jogoteca.py
@@ -1,116 +1,12 @@
-from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
+from flask import Flask
 from flask_mysqldb import MySQL
-from dao import JogoDao, UsuarioDao
-from models import Jogo, Usuario
-import os
-import time
 
 app = Flask(__name__)
-app.secret_key = 'alura'
-app.config['MYSQL_HOST'] = "127.0.0.1"
-app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "admin"
-app.config['MYSQL_DB'] = "jogoteca"
-app.config['MYSQL_PORT'] = 3306
-app.config['UPLOADS_PATH'] = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
+app.config.from_pyfile('config.py')
 
 db = MySQL(app)
 
-jogo_dao = JogoDao(db)
+from views import *
 
-usuario_dao = UsuarioDao(db)
-
-@app.route('/')
-def index():
-    lista = jogo_dao.listar()
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
-
-@app.route('/novo')
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
-
-@app.route('/criar', methods=['POST',])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-    jogo = Jogo(nome, categoria, console)
-    jogo_dao.salvar(jogo)
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOADS_PATH']
-    timestamp = time.time()
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-@app.route('/editar/<int:id>')
-def editar(id):
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('editar', id=id)))
-    jogo = jogo_dao.busca_por_id(id)
-    nome_imagem = recupera_imagem(id)
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=nome_imagem)
-
-@app.route('/atualizar', methods=['POST', ])
-def atualizar():
-    id = request.form['id']
-    nome = request. form['nome']
-    categoria = request. form['categoria']
-    console = request. form['console']
-    jogo = Jogo(nome, categoria, console, id)
-    jogo_dao.salvar(jogo)
-
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOADS_PATH']
-    timestamp = time.time()
-    deleta_arquivo(id)
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
-    return redirect(url_for('index'))
-
-@app.route('/deletar/<int:id>')
-def deletar(id):
-    jogo_dao.deletar(id)
-    flash('O jogo foi deletado com sucesso!')
-    return redirect(url_for('index'))
-
-@app.route('/uploads/<nome_arquivo>')
-def imagem(nome_arquivo):
-    return send_from_directory('uploads', nome_arquivo)
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima)
-
-
-@app.route('/autenticar', methods=['POST', ])
-def autenticar():
-    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
-    if usuario:
-        if usuario.senha == request.form['senha']:
-            session['usuario_logado'] = usuario.id
-            flash(usuario.nome + ' logou com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-    else:
-        flash('Não logado, tente novamente!')
-        return redirect(url_for('login'))
-
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-    flash('Nenhum usuário logado!')
-    return redirect(url_for('index'))
-
-def recupera_imagem(id):
-    for nome_arquivo in os.listdir(app.config['UPLOADS_PATH']):
-        if f'capa{id}' in nome_arquivo:
-            return nome_arquivo
-
-def deleta_arquivo(id):
-    arquivo = recupera_imagem(id)
-    os.remove(os.path.join(app.config['UPLOADS_PATH'], arquivo))
-
-app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
